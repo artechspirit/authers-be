068a8cd24639f3945be4578eeaa63771acb552c5
unknown <beta.priyoko@students.amikom.ac.id>
2025-05-23 12:07:32 +0700

    fix cookies

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -72,7 +72,7 @@ export const login = async (req: Request, res: Response) => {
     // Set cookie HttpOnly
     res.cookie("refreshToken", refreshToken, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: true,
       sameSite: "none",
       path: "/",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 hari
@@ -109,7 +109,7 @@ export const refreshToken = async (req: Request, res: Response) => {
     res
       .cookie("refreshToken", newRefreshToken, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
+        secure: true,
         sameSite: "none",
         path: "/",
         maxAge: 7 * 24 * 60 * 60 * 1000,
